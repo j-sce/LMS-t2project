5a73f4a32eaff8e3c527f0117b008924c843d176
j-sce <j.scerbicka@gmail.com>
2020-07-06 21:33:20 +0300

    Initial commit

---
+++ src/main/java/org/jtm/t2project/T2projectApplication.java
@@ -0,0 +1,13 @@
+package org.jtm.t2project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T2projectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T2projectApplication.class, args);
+	}
+
+}
