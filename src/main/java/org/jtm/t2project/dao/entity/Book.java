2c3f276655f972b9024701dd532efb16d973b7bd
Agnese <agnesegustaite@gmail.com>
2020-07-15 12:36:09 +0300

    Book entities update

--- src/main/java/org/jtm/t2project/dao/entity/Book.java
+++ src/main/java/org/jtm/t2project/dao/entity/Book.java
@@ -1,114 +1,118 @@
 package org.jtm.t2project.dao.entity;
 
 import javax.persistence.*;
+
+import org.hibernate.loader.custom.Return;
+
 import java.util.Set;
 
 @Entity
 @Table(name = "Books")
+
 public class Book {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+	private static final Object[] Book = null;
 
-    private Integer isbn;
-    private String title;
-    private String publisher;
-    private Integer year;
-    private Boolean available;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; 
+	
+	private int isbn; //ir arī iepriekšējā kodā
+	private String title; //ir arī iepriekšējā kodā
+	private String publisher; //ir arī iepriekšējā kodā
+	private int year; //ir arī iepriekšējā kodā
+	private boolean available; //ir arī iepriekšējā kodā
+	private String bookSubjects;
+	private String bookAuthors;
+	private int count; 
+	private int bookIssue;
+	
+	public String toString() {
+		return "Book [id=" + id + ", title=" + title + ", year=" + year + ", publisher=" + publisher + 
+				 + ", available=" + available + ", count=" + count + "]";
+	
+	public int getId() {
+		return id;
+	}
 
-    @ManyToMany
-    @JoinTable(
-            name = "book_subject_map",
-            joinColumns = @JoinColumn(name = "bookId"),
-            inverseJoinColumns = @JoinColumn(name = "subjectId")
-    )
-    private Set <Subject> bookSubjects;
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public int getIsbn() {
+		return isbn;
+	}
+	
+	public void setIsbn(int isbn) {
+		this.isbn = isbn;
+	}
+	
+	public String getTitle() {
+		return title;
+	}
 
-    @ManyToMany
-    @JoinTable(
-            name = "Book_Authors_Map",
-            joinColumns = @JoinColumn(name = "bookId"),
-            inverseJoinColumns = @JoinColumn(name = "authorId")
-    )
-    private Set <Author> bookAuthors;
+	public void setTitle(String title) {
+		this.title = title;
+	}
 
-//    @ManyToOne
-//    @JoinColumn(name = "Book_issue_id")
-//    private BookIssue bookIssue;
+	public String getPublisher() {
+		return publisher;
+	}
 
-    public Book() {
-    }
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+	
+	public int getYear() {
+		return year;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public void setYear(int year) {
+		this.year = year;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public boolean getAvailable() {
+		return available;
+	}
 
-    public Integer getIsbn() {
-        return isbn;
-    }
+	public void setAvailable(boolean available) {
+		this.available = available;
+	}
 
-    public void setIsbn(Integer isbn) {
-        this.isbn = isbn;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getPublisher() {
-        return publisher;
-    }
-
-    public void setPublisher(String publisher) {
-        this.publisher = publisher;
-    }
-
-    public Integer getYear() {
-        return year;
-    }
-
-    public void setYear(Integer year) {
-        this.year = year;
-    }
-
-    public Boolean getAvailable() {
-        return available;
-    }
-
-    public void setAvailable(Boolean available) {
-        this.available = available;
-    }
-
-    public Set <Subject> getBookSubjects() {
-        return bookSubjects;
-    }
-
-    public void setBookSubjects(Set <Subject> bookSubjects) {
-        this.bookSubjects = bookSubjects;
-    }
-
-    public Set <Author> getBookAuthors() {
+	public String getBookSubjects() { 
+		return bookSubjects; 
+	}
+				
+	public void setBookSubjects(String bookSubjects) {
+		this.bookSubjects = bookSubjects; 
+	}
+	
+	public String getBookAuthors() {
         return bookAuthors;
     }
 
-    public void setBookAuthors(Set <Author> bookAuthors) {
+    public void setBookAuthors(String bookAuthors) {
         this.bookAuthors = bookAuthors;
     }
+	
+	public int getCount() {
+		return count;
+	}
 
-//    public BookIssue getBookIssue() {
-//        return bookIssue;
-//    }
-//
-//    public void setBookIssue(BookIssue bookIssue) {
-//        this.bookIssue = bookIssue;
-//    }
+	public void setCount(int count) {
+		this.count = count;
+	}
+	
+	public int getBookIssue() {
+		return bookIssue;
+	}
+
+	public void setBookIssue(int bookIssue) {
+		this.bookIssue = bookIssue;
+	}
+	
+	public void String () {
+		Book [id=" + id + ", isbn=" + isbn + “, title=" + title + ", publisher=" + publisher + “, year=" + year + ", available=" + available + ", bookSubjects=“ + bookSubjects + “, bookAuthors=“ + bookAuthors + “, count=" + count + “, bookIssue=“ + bookIssue + “]”;
+				
+	}
 }
